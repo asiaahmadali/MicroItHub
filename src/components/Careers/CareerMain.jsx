044797d9b204ff8f0d76078e608d4e7e8c2947c7
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-08 18:08:44 +0500

    carrerr animations added

--- src/components/Careers/CareerMain.jsx
+++ src/components/Careers/CareerMain.jsx
@@ -5,12 +5,12 @@ function CareerMain()
 {
     return <>
        <div className=' h-[250px] md:h-[450px] flex flex-col md:p-[50px] p-[10px] mt-[60px] gap-[20px]'>
-            <div className='flex text-white bg-blue-500 items-center text-xl font-semibold p-[10px] rounded-md gap-[10px] w-full'>
+            <div  data-aos="zoom-in" className='flex text-white bg-blue-500 items-center text-xl font-semibold p-[10px] rounded-md gap-[10px] w-full'>
                    <FaCheckSquare></FaCheckSquare>
                    <p>Latest Jobs</p>
             </div>
 
-            <div className="border">
+            <div   data-aos="fade-right" className="border">
   
                 <div className="flex justify-evenly bg-gray-100 text-[16px] md:text-xl font-bold p-[10px] md:p-[30px] text-center items-center">
                     <h1 className="w-1/3">Job Title</h1>
